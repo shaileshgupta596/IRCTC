c1d00649631ad2843fa5a956ac51066e6546e1df
Shailesh Gupta <shaileshgupta596@gmail.com>
2024-09-08 21:56:06 +0530

    Develop Feature to find Routes between two stations

--- station/scripts/station_script.py
+++ station/scripts/station_script.py
@@ -1,8 +1,28 @@
 from station.models import Station, StationRoute
 
+def route_between_station(source, destination, visited, route_list):
+    if source == destination:
+        return True
+    
+    for route in source.station.routes.all():
+        if route not in visited:
+            visited.append(route)
+            if route_between_station(route, destination, visited, route_list):
+                route_list.append(route)
+                return True
+    
+    return False
+
+
 def run():
-    kalyan = StationRoute.objects.get(station__code="KYN")
-    print(kalyan.routes.all())
+    source = StationRoute.objects.get(station__code='CSMT')
+    destination = StationRoute.objects.get(station__code='AGC')
+    route_list = []
+    visited = [source]
+    print(route_between_station(source, destination, visited, route_list))
+    print(route_list)
+
+    # print(StationRoute.objects.get(station__code='BSL').routes.all())
 
 
 
